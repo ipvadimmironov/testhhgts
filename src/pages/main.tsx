eac665e16e24469637ae44a230ef1d511ef49c63
Vadim Mironov <vmironov1@gmail.com>
2024-05-13 18:15:35 +0300

    ready for publish. Build.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Persons, ProductService, equipmentStock } from '../service/EventService';
 // import { Button } from 'primereact/button';
 import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
