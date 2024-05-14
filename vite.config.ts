01c053a6d76304d2c83cbb1af27e5a2bdc1e95c5
Vadim Mironov <vmironov1@gmail.com>
2024-05-14 11:09:51 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/testhhgts/",
   server: {
     fs: {
       // Allow serving files from one level up to the project root
